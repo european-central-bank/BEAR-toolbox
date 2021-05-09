b2ae7305eb04346603fe59f0fb2b099d31fdac71
David Sampson <dsampson@mathworks.com>
2021-05-09 10:29:57 +0100

    results.xlsx is next to initexcel.m

--- tbx/bear/files/initexcel.m
+++ tbx/bear/files/initexcel.m
@@ -2,6 +2,6 @@ delete([pref.datapath filesep 'results' filesep pref.results_sub '.xlsx']);
 
 
 % then copy the blank excel file from the files to the data folder
-sourcefile=[pwd filesep 'results.xlsx'];
+sourcefile=[fileparts(mfilename('fullpath')) filesep 'results.xlsx'];
 destinationfile=[pref.datapath filesep 'results' filesep pref.results_sub '.xlsx'];
 copyfile(sourcefile,destinationfile);
