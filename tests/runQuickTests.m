f818be5149218072259a7df2e5609df045bd08b9
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-05-13 17:26:19 +0100

    Added Coverage Report

--- tests/runQuickTests.m
+++ tests/runQuickTests.m
@@ -1,5 +1,12 @@
 import matlab.unittest.TestSuite
+import matlab.unittest.TestRunner
+import matlab.unittest.plugins.CodeCoveragePlugin
+
 clear; clc;
 cd(fileparts(mfilename('fullpath')));
 suite = TestSuite.fromFile('replicationTests.m','Tag','QuickReplications');
-suite.run()
+runner = TestRunner.withTextOutput;
+runner.addPlugin(CodeCoveragePlugin.forFolder(bearroot(), 'IncludingSubfolders', true))
+result = runner.run(suite);
+tb = table(result);
+disp(tb);
