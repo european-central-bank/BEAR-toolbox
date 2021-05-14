d24d2fdf739913f24f1949cf2ff008fa257dfd28
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-05-14 18:07:08 +0100

    Added all-tests options

---
+++ tests/runAllTests.m
@@ -0,0 +1,12 @@
+import matlab.unittest.TestSuite
+import matlab.unittest.TestRunner
+import matlab.unittest.plugins.CodeCoveragePlugin
+
+clear; clc;
+cd(fileparts(mfilename('fullpath')));
+suite = TestSuite.fromFile('replicationTests.m');
+runner = TestRunner.withTextOutput;
+runner.addPlugin(CodeCoveragePlugin.forFolder(bearroot(), 'IncludingSubfolders', true))
+result = runner.run(suite);
+tb = table(result);
+disp(tb);
