0eab26ae6de5a7d7686d9c26bd67d4891de91bcb
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-05-28 17:52:01 +0100

    Added results path pref in two of the settings for testing

--- tests/bear_settings_test.m
+++ tests/bear_settings_test.m
@@ -50,6 +50,7 @@ const=1;
 pref.datapath = bearroot(); % fileparts(mfilename('fullpath')); % next to settings
 pref.excelFile = excelPath;
 % excel results file name
+pref.results_path = fullfile(fileparts(mfilename('fullpath')),'results');
 pref.results_sub='results_test_data_temp';
 % to output results in excel
 pref.results=0;
