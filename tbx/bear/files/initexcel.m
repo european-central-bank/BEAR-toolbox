bf6cf4599943f28c30cf5c3fdfd4deaebf7f32fd
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-05-28 19:40:07 +0100

    Fixing bug in initexcel

--- tbx/bear/files/initexcel.m
+++ tbx/bear/files/initexcel.m
@@ -8,7 +8,7 @@ end
 % then copy the blank excel file from the files to the data folder
 sourcefile = [fileparts(mfilename('fullpath')) filesep 'results.xlsx'];
 destinationfile = fullfile(pref.results_path, [pref.results_sub '.xlsx']);
-if exist(resultsFile, 'dir') == 0
+if exist(pref.results_path, 'dir') == 0
     mkdir(pref.results_path)
 end
 copyfile(sourcefile,destinationfile);
