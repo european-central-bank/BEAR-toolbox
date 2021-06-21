dee124f2b6014e83cedff7771579cc411d7e4613
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-06-21 14:57:49 +0100

    Added settings to quick tests

--- tests/runQuickTests.m
+++ tests/runQuickTests.m
@@ -3,7 +3,10 @@ import matlab.unittest.TestRunner
 import matlab.unittest.plugins.CodeCoveragePlugin
 
 clear; clc;
-suite = TestSuite.fromFile('replicationTests.m','Tag','QuickReplications');
+suite1 = TestSuite.fromFile('replicationTests.m','Tag','QuickReplications');
+suite2 = TestSuite.fromFile('tSettings.m');
+suite = [suite1, suite2];
+
 runner = TestRunner.withTextOutput;
 runner.addPlugin(CodeCoveragePlugin.forFolder(bearroot(), 'IncludingSubfolders', true))
 result = runner.run(suite);
