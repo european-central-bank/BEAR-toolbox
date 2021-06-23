687496600994862c1a407e3bfd954d52051ce565
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-06-23 09:36:55 +0100

    Added levels enum

---
+++ tbx/bear/+bear/Levels.m
@@ -0,0 +1,10 @@
+classdef Levels < uint8
+    %LEVELS re-tranform transformed variables
+    
+    enumeration
+        No_retransformation (0)
+        Cumsum              (1)         
+        Exp_cumsum          (2)
+    end
+
+end
