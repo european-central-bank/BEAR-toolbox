62f0302e50421d7bb38958dfec95a17d3d21f015
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-06-23 13:18:17 +0100

    Minor changes to doc

--- tbx/bear/+bear/Prior.m
+++ tbx/bear/+bear/Prior.m
@@ -12,7 +12,7 @@ classdef Prior < uint8
         Independent_Normal_Wishart_S0_ID (32) % 32=Independent Normal-Wishart(S0 as identity)
         Normal_diffuse                   (41) % 41=Normal-diffuse 
         Dummy_observations               (51) % 51=Dummy observations
-        Mean_adjusted                    (61)% 61=Mean-adjusted
+        Mean_adjusted                    (61) % 61=Mean-adjusted
     
     end
 
