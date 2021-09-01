564ace6d366aecea43551b87781f2a3ef40122f3
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-09-01 12:08:18 +0100

    Cleaned MFVAR class, need original input file

--- tbx/bear/+bear/+settings/MFVARsettings.m
+++ tbx/bear/+bear/+settings/MFVARsettings.m
@@ -10,21 +10,8 @@ classdef MFVARsettings < bear.settings.BASEsettings
     %
     %   BEARsettings('mfvar', ExcelPath = 'path/To/file.xlsx')
     %
-    % MFVARsettings Properties:
-    %    prior           - Selected prior
-    %    PriorExcel      - Select individual priors
-    %    priorsexogenous - Gibbs sampler burn-in iterations    
-    %    It              - Gibbs sampler iterations
-    %    Bu              - Gibbs sampler burn-in iterations
-    %    hogs            - grid search
-    %    bex             - block exogeneity
-    %    scoeff          - apply sum of coefficients
-    %    iobs            - initial observation
-    %    lrp             - Long run prior option
-    %    priorf          - scale of prior of factor f
-    %    strctident      - strctident    
+    % MFVARsettings Properties:  
     %    ar              - auto-regressive coefficients
-    %    alpha0          - hyperparameter
     %    lambda1         - hyperparameter
     %    lambda2         - hyperparameter
     %    lambda3         - hyperparameter
@@ -33,44 +20,6 @@ classdef MFVARsettings < bear.settings.BASEsettings
     %    lambda6         - hyperparameter
     %    lambda7         - hyperparameter
     %    lambda8         - hyperparameter
-    
-    properties    
-%         % Selected prior:
-%         % 11=Minnesota (univariate AR), 12=Minnesota (diagonal VAR estimates), 13=Minnesota (full VAR estimates)
-%         % 21=Normal-Wishart(S0 as univariate AR), 22=Normal-Wishart(S0 as identity)
-%         % 31=Independent Normal-Wishart(S0 as univariate AR), 32=Independent Normal-Wishart(S0 as identity)
-%         % 41=Normal-diffuse
-%         % 51=Dummy observations
-%         % 61=Mean-adjusted
-%         prior (1,1) bear.PRIORtype = 61;
-%         % switch to Excel interface
-%         PriorExcel (1,1) logical = false; % set to 1 if you want individual priors, 0 for default
-%         %switch to Excel interface for exogenous variables
-%         priorsexogenous (1,1) logical = false; % set to 1 if you want individual priors, 0 for default
-%         % total number of iterations for the Gibbs sampler
-%         It=1000;
-%         % number of burn-in iterations for the Gibbs sampler
-%         Bu=500;
-%         % hyperparameter optimisation by grid search (1=yes, 0=no)
-%         hogs (1,1) logical = 0;
-%         % block exogeneity (1=yes, 0=no)
-%         bex  (1,1) logical = 0;
-%         % sum-of-coefficients application (1=yes, 0=no)
-%         scoeff (1,1) logical =0;
-%         % dummy initial observation application (1=yes, 0=no)
-%         iobs (1,1) logical =0;
-%         % Long run prior option
-%         lrp=0;        
-%         % create H matrix for the long run priors
-%         % now taken from excel loadH.m
-%         % H=[1 1 0 0;-1 1 0 0;0 0 1 1;0 0 -1 1];
-%         % (61=Mean-adjusted BVAR) Scale up the variance of the prior of factor f
-%         priorf=100;        
-%         % strctident
-%         strctident
-%         % hyperparameter: alpha0 Setting or result?
-%         alpha0=1000;
-    end
     
     properties % Hyperparameters
         % Autoregressive coefficient: ar
@@ -98,8 +47,6 @@ classdef MFVARsettings < bear.settings.BASEsettings
         function obj = MFVARsettings(excelPath, varargin)
 
             obj@bear.settings.BASEsettings(7, excelPath)
-
-%             obj = obj.setStrctident(obj.IRFt);
             
             obj = parseBEARSettings(obj, varargin{:});
             
@@ -107,32 +54,4 @@ classdef MFVARsettings < bear.settings.BASEsettings
         
     end
 
-%     methods (Access = protected)
-% 
-%         function obj = checkIRFt(obj, value)
-%             % we could call superclass method to combine effect
-%             obj = checkIRFt@bear.settings.BASEsettings(obj, value);
-%             obj = obj.setStrctident(value);
-%         end
-%         
-%     end
-% 
-%     methods (Access = private)
-% 
-%         function obj = setStrctident(obj, value)
-%             
-%             switch value
-%                 case 4
-%                     obj.strctident = bear.settings.StrctidentIRFt4;
-%                 case 5                    
-%                     obj.strctident = bear.settings.StrctidentIRFt5;
-%                 case 6
-%                     obj.strctident = bear.settings.StrctidentIRFt6;                
-%                 otherwise
-%                     obj.strctident = bear.settings.Strctident.empty();
-%             end
-%             
-%         end
-% 
-%     end
 end
