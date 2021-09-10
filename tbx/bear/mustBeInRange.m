cac087fdc7d64b67bf293de42dfc8eb14eca3548
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-09-10 15:07:47 +0100

    Comment cleaning

--- tbx/bear/mustBeInRange.m
+++ tbx/bear/mustBeInRange.m
@@ -1,8 +1,6 @@
 function mustBeInRange(prop, lower, upper)
-% if verLessThan('MATLAB', '9.9')
-%     mustBeInRange(prop, lower, upper)
-% else
-    mustBeGreaterThanOrEqual(prop, lower)
-    mustBeLessThanOrEqual(prop, upper) 
-% end
+
+mustBeGreaterThanOrEqual(prop, lower)
+mustBeLessThanOrEqual(prop, upper)
+
 end
