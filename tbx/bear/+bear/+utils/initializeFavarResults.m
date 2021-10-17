b69cbdcffd52a7875832137f6b8272bae7de343c
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-10-17 12:40:33 +0100

    added favar generation fixes for BEAR main

--- tbx/bear/+bear/+utils/initializeFavarResults.m
+++ tbx/bear/+bear/+utils/initializeFavarResults.m
@@ -1,18 +1,34 @@
 function favar = initializeFavarResults(opts)
 % INITIALIZESTRCIDENT this function initalizes a strcident struct to store
 % the results based on the already existing fields on the settings objet
-favar = struct();
-p = opts.getActiveProperties;
-for i = 1 : length(p)
-    switch p{i}
-        case "HD"
-            favar.HD   = bear.utils.initializeFavarResults(opts.HD);
-        case "IRF"
-            favar.IRF  = bear.utils.initializeFavarResults(opts.IRF);
-        case "FEVD"
-            favar.FEVD = bear.utils.initializeFavarResults(opts.FEVD);
-        otherwise
-            favar.(p{i}) = opts.(p{i});
-    end
+arguments
+    opts (1,1) bear.settings.BASEsettings
 end
+
+favar.FAVAR     = false;
+favar.HD.plot   = false;
+favar.IRF.plot  = false;
+favar.FEVD.plot = false;
+
+if isprop(opts, 'favar') && isa(opts.favar, 'bear.settings.FAVARsettings')
+    favar = initalizeFavar(opts.favar);
+end
+end
+
+function favar = initalizeFavar(favarOpt)
+
+p = favarOpt.getActiveProperties;
+    for i = 1 : length(p)
+        switch p{i}
+            case "HD"
+                favar.HD   = initalizeFavar(favarOpt.HD);
+            case "IRF"
+                favar.IRF  = initalizeFavar(favarOpt.IRF);
+            case "FEVD"
+                favar.FEVD = initalizeFavar(favarOpt.FEVD);
+            otherwise
+                favar.(p{i}) = favarOpt.(p{i});
+        end
+    end
+
 end
