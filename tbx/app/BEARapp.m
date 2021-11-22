b49a6201bbcd40b70adb3d59dc9e466c9b506db4
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-11-22 00:47:21 +0000

    Removed new PV pair synthax

--- tbx/app/BEARapp.m
+++ tbx/app/BEARapp.m
@@ -1,7 +1,7 @@
 function BEARapp()
 % Launch the appropriate version of the BEAR app
 if verLessThan('matlab','9.9')
-    eval('BEARapp20a');
+    eval('bear.app.BEARapp20a');
 else
-    eval('BEARapp21a');
+    eval('bear.app.BEARapp21a');
 end
