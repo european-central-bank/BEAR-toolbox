bc01a8a9d39cc5fff8b9950045ede8a322b4265f
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-11-25 17:39:04 +0000

    White space removal in tApp

--- tests/tApp.m
+++ tests/tApp.m
@@ -14,32 +14,36 @@ classdef tApp < matlab.uitest.TestCase
     end
     
     methods (Test)
+        
         function tFrequency(tc)
-           tc.choose(tc.App.frequency_cp, 'monthly');
-           tc.press(tc.App.QuickExporttoWorkspaceButton)
-           opts = evalin('base', 'opts');
-           tc.verifyEqual(opts.frequency, 3);
-           
-           tc.choose(tc.App.frequency_cp, 'quarterly');
-           tc.press(tc.App.QuickExporttoWorkspaceButton)
-           opts = evalin('base', 'opts');
-           tc.verifyEqual(opts.frequency, 2);  
-           
-           tc.choose(tc.App.frequency_cp, 'yearly');
-           tc.press(tc.App.QuickExporttoWorkspaceButton)
-           opts = evalin('base', 'opts');
-           tc.verifyEqual(opts.frequency, 1);
-           
-           tc.choose(tc.App.frequency_cp, 'weekly');
-           tc.press(tc.App.QuickExporttoWorkspaceButton)
-           opts = evalin('base', 'opts');
-           tc.verifyEqual(opts.frequency, 4);  
-           
-           tc.choose(tc.App.frequency_cp, 'daily');
-           tc.press(tc.App.QuickExporttoWorkspaceButton)
-           opts = evalin('base', 'opts');
-           tc.verifyEqual(opts.frequency, 5);  
+            
+            tc.choose(tc.App.frequency_cp, 'monthly');
+            tc.press(tc.App.QuickExporttoWorkspaceButton)
+            opts = evalin('base', 'opts');
+            tc.verifyEqual(opts.frequency, 3);
+            
+            tc.choose(tc.App.frequency_cp, 'quarterly');
+            tc.press(tc.App.QuickExporttoWorkspaceButton)
+            opts = evalin('base', 'opts');
+            tc.verifyEqual(opts.frequency, 2);
+            
+            tc.choose(tc.App.frequency_cp, 'yearly');
+            tc.press(tc.App.QuickExporttoWorkspaceButton)
+            opts = evalin('base', 'opts');
+            tc.verifyEqual(opts.frequency, 1);
+            
+            tc.choose(tc.App.frequency_cp, 'weekly');
+            tc.press(tc.App.QuickExporttoWorkspaceButton)
+            opts = evalin('base', 'opts');
+            tc.verifyEqual(opts.frequency, 4);
+            
+            tc.choose(tc.App.frequency_cp, 'daily');
+            tc.press(tc.App.QuickExporttoWorkspaceButton)
+            opts = evalin('base', 'opts');
+            tc.verifyEqual(opts.frequency, 5);
+            
         end
+        
     end
-
+    
 end
