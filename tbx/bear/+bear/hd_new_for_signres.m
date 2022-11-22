5f16876b1b96beace9429922eaf03ef17e79a88b
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2022-11-22 11:45:34 +0000

    hd_fixes

--- tbx/bear/+bear/hd_new_for_signres.m
+++ tbx/bear/+bear/hd_new_for_signres.m
@@ -31,11 +31,11 @@ elseif IRFt==1
 end
 
 %===============================================
-Bfull=reshape(beta,k,n);                    %get the Bfull matrix
-B=Bfull(1:n*p,:);                           %drop the coefficients for all exogenous variables from the matrix
-Bcomp = [B'; eye(n*(p-1)) zeros(n*(p-1),n)];%put into companion form
+Bfull=reshape(beta,k,n);                     %get the Bfull matrix
+B=Bfull(1:n*p,:);                            %drop the coefficients for all exogenous variables from the matrix
+Bcomp = [B'; eye(n*(p-1)) zeros(n*(p-1),n)]; %put into companion form
 EPS=Y-X*Bfull;                               %get reduced form residuals
-ETA=(D\EPS');                        %get structural shocks
+ETA=(D\EPS');                                %get structural shocks
 
 
 %% Compute historical decompositions
@@ -43,17 +43,12 @@ ETA=(D\EPS');                        %get structural shocks
 % Contribution of each shock
 aux_D = zeros(n*p,n); %auxilary D matrix, that is consistent with companion matrix
 aux_D(1:n,:) = D; %set the first entrys equal to Dinv
-Selec = [eye(n) zeros(n,(p-1)*n)]; %selection matrix
 HDestimates_store = zeros(p*n,T+1,n); %cell aray to store results
 HDestimates = zeros(n,T+1,n);
+ETAcomp = zeros(n,T+1,n); %structural shock matrix that is consistent with companion form
 for j=1:n % for each variable
-    ETAcomp = zeros(n,T+1); %structural shock matrix that is consistent with companion form
     if j <= identified %if j is an identified shock
-        ETAcomp(j,2:end) = ETA(j,:); %fill in the entry for shock n, leave the first entry blank
-    end
-    for i = 2:T+1
-        HDestimates_store(:,i,j) = aux_D*ETAcomp(:,i) + Bcomp*HDestimates_store(:,i-1,j); %recursively sum over shock impulse at period i on variable j and the previous period
-        HDestimates(:,i,j) =  Selec*HDestimates_store(:,i,j); %select the entry corresponding to the current period
+        ETAcomp(j,2:end,j) = ETA(j,:); %fill in the entry for shock n, leave the first entry blank
     end
 end
 
@@ -62,22 +57,14 @@ HDinitial_storage   = zeros(p*n,T+1);
 HDinitial_estimates = zeros(n, T+1);
 Xnoexo = X(:,1:n*p);
 HDinitial_storage(:,1) = Xnoexo(1,:)'; %set the initial values to the first row of X (n*P)+exo
-HDinitial_estimates(:,1) = Selec*HDinitial_storage(:,1); %select the initial values for the first n variables (i.e. the values at Y_{t-1}
-for i = 2:T+1 %loop over periods and compute the impact of the initial conditions recursively
-    HDinitial_storage(:,i) = Bcomp*HDinitial_storage(:,i-1); %compute the impact of those values (which in principle consist of past shocks)
-    HDinitial_estimates(:,i) = Selec*HDinitial_storage(:,i);
-end
+HDinitial_estimates(:,1) = HDinitial_storage(1:n,1); %select the initial values for the first n variables (i.e. the values at Y_{t-1}
 
 %  Contribution of the Constant
-HDconstant_storage = zeros(p*n,T+1);
-HDconstant_estimates = zeros(n, T+1);
-Coefficients = zeros(p*n,1);
 if const==1
+    HDconstant_storage = zeros(p*n,T+1);
+    HDconstant_estimates = zeros(n, T+1);
+    Coefficients = zeros(p*n,1);
     Coefficients(1:n,:) = Bfull(n*p+1,:);
-    for i = 2:T+1 %loop over periods
-        HDconstant_storage(:,i) = Coefficients + Bcomp*HDconstant_storage(:,i-1);
-        HDconstant_estimates(:,i) = Selec * HDconstant_storage(:,i);
-    end
 end
 
 % Contribution of exogenous variables, no lags of exogenous variables just
@@ -85,83 +72,75 @@ end
 if (m-const) >= 1
     HDexo_storage = zeros(p*n,T+1);
     HDexo_estimates = zeros(n,T+1);
-    %     Coefficients_exo = zeros(p*n,(m-const)*(1+p));
     Coefficients_exo = zeros(p*n,m-const);
     data_exocut=X(:,n*p+const+1:end); % take exogenous data from X
-    %     Coefficients_exo(1:n,:) = Bfull(n*p+const+1:end,:)'; %get the corresponding coefficients
     Coefficients_exo(1:n,:) = Bfull(n*p+const+1:end,:)'; %get the corresponding coefficients
-    for i = 2:T+1
+end
+
+for i = 2:T+1 %loop over periods and compute the impact of the initial conditions recursively
+    HDestimates_store(:,i,:) = pagemtimes(aux_D, ETAcomp(:,i,:)) + pagemtimes(Bcomp, HDestimates_store(:,i-1,:)); %recursively sum over shock impulse at period i on variable j and the previous period
+    HDestimates(1:n,i,:) =  HDestimates_store(1:n,i,:); %select the entry corresponding to the current period
+
+    HDinitial_storage(:,i) = Bcomp*HDinitial_storage(:,i-1); %compute the impact of those values (which in principle consist of past shocks)
+    HDinitial_estimates(:,i) = HDinitial_storage(1:n,i);
+
+    if const==1
+        HDconstant_storage(:,i) = Coefficients + Bcomp*HDconstant_storage(:,i-1);
+        HDconstant_estimates(:,i) = HDconstant_storage(1:n,i);
+    end
+
+    if (m-const) >= 1
         HDexo_storage(:,i) = Coefficients_exo*data_exocut(i-1,:)' + Bcomp*HDexo_storage(:,i-1);
-        HDexo_estimates(:,i) = Selec * HDexo_storage(:,i);
+        HDexo_estimates(:,i) = HDexo_storage(1:n,i);
     end
 end
+
 
 %% put these values into the corresponding cell for hd_estimates such that
 % for variable x (hd_estimates(x,n+1)) = HDinitial_estimates(x,:)
 % for variable x (hd_estimates(x,n+1)) = HDconstant_estimates(x,:)
 %reorganize storage
-for jj=1:n %for variables
-    for kk=1:T+1 %for periods
-        for ii=1:n %for shock contributions
-            hd_estimates2{ii,jj}(1,kk) = HDestimates(jj,kk,ii);
-        end
-        hd_estimates2{n+1,jj}(1,kk)=HDinitial_estimates(jj,kk);
-        if const==1
-            hd_estimates2{n+2,jj}(1,kk)=HDconstant_estimates(jj,kk);
-        else
-            hd_estimates2{n+2,jj}(1,kk)=0;
-        end
-        if (m-const) >= 1
-            hd_estimates2{n+3,jj}(1,kk)=HDexo_estimates(jj,kk);
-        end
+idx = 1:T+1;
+for jj = 1:n
+    for ii = 1:n
+        hd_estimates2{ii,jj} = HDestimates(jj,idx,ii);
+    end
+    hd_estimates2{n+1,jj}=HDinitial_estimates(jj,idx);
+    if const == 1
+        hd_estimates2{n+2,jj} = HDconstant_estimates(jj,idx);
+    else
+        hd_estimates2{n+2,jj} = zeros(1, T+1);
+    end
+    if (m-const) >= 1
+        hd_estimates2{n+3,jj} = HDexo_estimates(jj,idx);
     end
 end
 
-
 HDsum = zeros(T+1,n); %if we sum over all variables this should give Y
+Exosum = zeros(T+1,n); %if we sum over all variables this should give Y
 for jj=1:n %loop over variables (columns)
-    sumvariable = zeros(1,T+1);
-    for kk=1:T+1 %loop over periods
-        sumperiod=0;
-        for ii=1:contributors %loop over contributors (rows)
-            value = hd_estimates2{ii,jj}(1,kk);
-            sumperiod = sumperiod+value;
-        end
-        sumvariable(1,kk) = sumperiod;
-    end
-    HDsum(:,jj)=sumvariable(1,:);
+    summatrix = vertcat(hd_estimates2{1:contributors,jj});
+    HDsum(:,jj)=sum(summatrix);
+
+    %finally substract the sum of the contribution of the
+    %exogenous, constant,initial conditions from Y to get the
+    %part that was left to be explained by the shocks (for plotting reasons)
+    summatrix = vertcat(hd_estimates2{n+1:contributors,jj});
+    Exosum(:,jj)=sum(summatrix);
 end
 
 %determine the unexplained part (if model is not fully identified)
 aux = zeros(1,n);
 unexplained = Y-HDsum(2:end,:);
 unexplained = [aux; unexplained];
-for jj=1:n
-    hd_estimates2{contributors+1,jj}=unexplained(:,jj)';
-end
-
-%finally substract the sum of the contribution of the
-%exogenous, constant,initial conditions from Y to get the
-%part that was left to be explained by the shocks (for plotting reasons)
-Exosum = zeros(T+1,n); %if we sum over all variables this should give Y
-for jj=1:n %loop over variables (columns)
-    sumvariable = zeros(1,T+1);
-    for kk=1:T+1 %loop over periods
-        sumperiod=0;
-        for ii=n+1:contributors %loop over contributors (rows)
-            value = hd_estimates2{ii,jj}(1,kk);
-            sumperiod = sumperiod+value;
-        end
-        sumvariable(1,kk) = sumperiod;
-    end
-    Exosum(:,jj)=sumvariable(1,:);
-end
 
 %determine the part that was left to be explained by the shocks
 aux = zeros(1,n);
 tobeexplained = Y - Exosum(2:end,:);
 tobeexplained = [aux; tobeexplained];
+
 for jj=1:n
+    hd_estimates2{contributors+1,jj}=unexplained(:,jj)';
     hd_estimates2{contributors+2,jj}=tobeexplained(:,jj)';
 end
 
@@ -173,4 +152,3 @@ for jj=1:n
         hd_estimates{ii,jj}(2,:)=hd_estimates2{ii,jj}(2:end);
     end
 end
-
--- tbx/bear/+bear/hdecomp_inc_exo.m
+++ tbx/bear/+bear/hdecomp_inc_exo.m
@@ -1,7 +1,4 @@
 function [hd_record,favar]=hdecomp_inc_exo(beta_gibbs,D_record,It,Bu,Y,X,n,m,p,k,T,data_exo,exo,endo,const,IRFt,strctident,favar)
-
-
-
 % function [hd_record]=bear.hdecomp(beta_gibbs,sigma_gibbs,D_record,It,Bu,Y,X,n,m,p,k,T)
 % runs the gibbs sampler to obtain draws from the posterior distribution of historical decomposition
 % inputs:  - matrix 'beta_gibbs': record of the gibbs sampler draws for the beta vector
@@ -37,7 +34,6 @@ end
 % first create the hd_record and temp cells
 contributors = n + 1 + length(exo) + 1 ; %variables + constant + exogenous + initial conditions
 hd_record=cell(contributors+2,n);
-HDstorage = cell(It-Bu,1);
 
 D_gibbs=reshape(D_record,n,n,It-Bu);
 % recall X and Y from the sampling process in this case, analogue to beta and sigma
@@ -61,35 +57,28 @@ for ii=1:It-Bu
         Y=squeeze(Ygibbs(:,:,ii));
     end
     
-    [hd_estimates]=bear.hd_new_for_signres(const,exo,beta,k,n,p,D,m,T,X,Y,IRFt,labels);
-    HDstorage{ii,1}=hd_estimates;
+    [hd_estimates]=hd_new_for_signres(const,exo,beta,k,n,p,D,m,T,X,Y,IRFt,labels);
     
     if favar.FAVAR==1 && favar.HDplot==1
         L_g=squeeze(Lgibbs(:,:,ii));
         [favar,favar_hd_estimates]=bear.favar_hdestimates(favar,hd_estimates,n,IRFt,endo,strctident,L_g);
         favar_HDstorage{ii,1}=favar_hd_estimates;
     end
-end
 
-%% reorganize historical decomposition
-for ii=1:It-Bu %loop over draws
+    % reorganize historical decomposition
     for kk=1:contributors+2 %loop over contributors
         for ll=1:n %loop over variables
-            hd_record{kk,ll}(ii,:) = HDstorage{ii,1}{kk,ll}(2,:);
+            hd_record{kk,ll}(ii,:) = hd_estimates{kk,ll}(2,:);
+            if favar.FAVAR==1 && favar.HDplot==1
+                for ww=1:favar.npltX
+                    favar_hd_record{kk,ll,ww}(ii,:)=favar_HDstorage{ii,1}{kk,ll,ww};
+                end
+            end
         end
     end
 end
 
 if favar.FAVAR==1 && favar.HDplot==1
-    for ii=1:It-Bu %loop over draws
-        for kk=1:contributors+2 %loop over contributors
-            for ll=1:n %loop over variables
-            for ww=1:favar.npltX
-                favar_hd_record{kk,ll,ww}(ii,:)=favar_HDstorage{ii,1}{kk,ll,ww};
-            end
-            end
-        end
-    end
     % save output
     favar.HD.favar_hd_record=favar_hd_record;
 end
