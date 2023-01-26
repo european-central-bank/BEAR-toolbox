527e130764a205b66e3926d4b53712aaa2e38ce2
ebenetce <ebenetce@host.docker.internal>
2023-01-26 12:11:19 +0000

    Added tests for strctident new setting

--- tests/tSettings.m
+++ tests/tSettings.m
@@ -1,12 +1,12 @@
 classdef tSettings < matlab.unittest.TestCase
-    
+
     methods(Test, TestTags = {'Git'})
-        
+
         function tDefaults(tc)
             s = BEARsettings(1);
             tc.verifyEqual(s.excelFile, fullfile(bearroot(),'default_bear_data.xlsx'))
         end
-        
+
         function tSetterFcnByNumber(tc)
             s = BEARsettings(1, 'ExcelFile', 'data.xlsx');
             tc.verifyClass(s, 'bear.settings.OLSsettings')
@@ -26,8 +26,8 @@ classdef tSettings < matlab.unittest.TestCase
 
             s = BEARsettings(6, 'ExcelFile', "data.xlsx");
             tc.verifyClass(s, 'bear.settings.TVPsettings')
-            tc.verifyEqual(s.VARtype, bear.VARtype(6))            
-            
+            tc.verifyEqual(s.VARtype, bear.VARtype(6))
+
             s = BEARsettings(7, 'ExcelFile', "data.xlsx");
             tc.verifyClass(s, 'bear.settings.MFVARsettings')
             tc.verifyEqual(s.VARtype, bear.VARtype(7))
@@ -47,8 +47,8 @@ classdef tSettings < matlab.unittest.TestCase
             tc.verifyClass(s, 'bear.settings.SVsettings')
 
             s = BEARsettings("TVP", 'ExcelFile', "data.xlsx");
-            tc.verifyClass(s, 'bear.settings.TVPsettings')          
-            
+            tc.verifyClass(s, 'bear.settings.TVPsettings')
+
             s = BEARsettings("MFVAR", 'ExcelFile', "data.xlsx");
             tc.verifyClass(s, 'bear.settings.MFVARsettings')
         end
@@ -92,82 +92,110 @@ classdef tSettings < matlab.unittest.TestCase
             s = BEARsettings(2, 'ExcelFile', 'data.xlsx', 'IRFt', 6);
             tc.verifyClass(s.strctident, 'bear.settings.strctident.StrctidentIRFt6');
         end
-        
+
         function tBVARHyperparamLimits(tc)
             param = ["ar";"lambda1";"lambda2";"lambda3";"lambda4";"lambda5";"lambda6";"lambda7";"lambda8"];
             lowerBound   = [-inf; 0; 0.1; 0; 0; 0; 0; 0; 0];
             defaultValue = [0.8; 0.1; 0.5; 1; 100; 0.001; 0.1; 0.001; 1];
             upperBound   = [inf; inf; inf; 2; inf; 1; inf; inf; 100];
-            
+
             t = table(param, lowerBound, defaultValue, upperBound);
             s = BEARsettings('bvar', 'ExcelFile', 'data.xlsx');
-            
+
             tc.icheckHyperparamLimits(s, t)
         end
-        
+
         function tPANELHyperparamLimits(tc)
             param = ["ar";"lambda1";"lambda2";"lambda3";"lambda4";"s0";"v0";"alpha0";"delta0";"gamma";"a0";"b0";"rho";"psi"];
             lowerBound   = [-inf; 0; 0.1; 1; 0; -inf; -inf; -inf; -inf; -inf; -inf; -inf; -inf; -inf];
             defaultValue = [0.8; 0.1; 0.5; 1; 100; 0.001; 0.001; 1000; 1; 0.85; 1000; 1; 0.75; 0.1];
             upperBound   = [inf; inf; inf; 2; inf; inf; inf; inf; inf; inf; inf; inf; inf; inf];
-            
+
             t = table(param, lowerBound, defaultValue, upperBound);
             s = BEARsettings('panel', 'ExcelFile', 'data.xlsx');
-            
+
             tc.icheckHyperparamLimits(s, t)
         end
-        
+
         function tSVHyperparamLimits(tc)
             param = ["ar";"lambda1";"lambda2";"lambda3";"lambda4";"lambda5";"gamma";"alpha0";"delta0";"gamma0";"zeta0"];
             lowerBound   = [-inf; 0; 0.1; 1; 0; 0; -inf; -inf; -inf; -inf; -inf];
             defaultValue = [0.8; 0.2; sqrt(2)/2; 1; 100; 0.001; 1; 0.001; 0.001; 0; 10000];
             upperBound   = [inf; inf; inf; 2; inf; 1; inf; inf; inf; inf; inf];
-            
+
             t = table(param, lowerBound, defaultValue, upperBound);
             s = BEARsettings('sv', 'ExcelFile', 'data.xlsx');
-            
+
             tc.icheckHyperparamLimits(s, t)
         end
-        
+
         function tTVPHyperparamLimits(tc)
             param = ["gamma";"alpha0";"delta0"];
             lowerBound   = [-inf; -inf; -inf];
             defaultValue = [0.85; 0.001; 0.001];
             upperBound   = [inf; inf; inf];
-            
+
             t = table(param, lowerBound, defaultValue, upperBound);
             s = BEARsettings('tvp', 'ExcelFile', 'data.xlsx');
-            
+
             tc.icheckHyperparamLimits(s, t)
         end
-        
+
         function tMFVARHyperparamLimits(tc)
             param = ["ar";"lambda1";"lambda2";"lambda3";"lambda4";"lambda5";"lambda6";"lambda7";"lambda8"];
             lowerBound   = [-inf; 0; 0.1; 1; 0; -inf; 0; 0; -inf];
             defaultValue = [0.9; 0.1; 3.4; 1; 3.4; 14.763158; 1; 0.01; 1];
             upperBound   = [inf; inf; inf; 2; inf; inf; inf; inf; inf];
-            
+
             t = table(param, lowerBound, defaultValue, upperBound);
             s = BEARsettings('mfvar', 'ExcelFile', 'data.xlsx');
-            
+
             tc.icheckHyperparamLimits(s, t)
         end
-        
+
         function tMustBeInRange(tc)
-            
-           opts = BEARsettings(2,'ExcelFile','data.xlsx');
-            
-            fcn = @() setProp(opts, 'lambda3',10);            
+
+            opts = BEARsettings(2,'ExcelFile','data.xlsx');
+
+            fcn = @() setProp(opts, 'lambda3',10);
             tc.verifyError(fcn, 'MATLAB:validators:mustBeLessThanOrEqual')
-            
-            fcn = @() setProp(opts, 'lambda3',-10);            
-            tc.verifyError(fcn, 'MATLAB:validators:mustBeGreaterThanOrEqual') 
-            
+
+            fcn = @() setProp(opts, 'lambda3',-10);
+            tc.verifyError(fcn, 'MATLAB:validators:mustBeGreaterThanOrEqual')
+
+        end
+
+        function tStrcident(tc)
+
+            opts = BEARsettings(2,'ExcelFile','data.xlsx');
+            tc.verifyEmpty(opts.strctident)
+            opts.IRFt = 5;
+            opts.strctident.Instrument = 'test';
+            opts.IRFt = 6;
+            tc.verifyEqual(opts.strctident.Instrument, 'test');
+            opts.IRFt = 4;
+            opts.IRFt = 6;
+            tc.verifyEqual(opts.strctident.Instrument, 'MHF');
+
+            opts = BEARsettings(1,'ExcelFile','data.xlsx');
+            tc.verifyEmpty(opts.strctident)
+            opts.IRFt = 4;
+            opts.strctident.CorrelShock = 'test';
+            opts.IRFt = 6;
+            tc.verifyEqual(opts.strctident.CorrelShock, 'test');
+
+            opts = BEARsettings(5,'ExcelFile','data.xlsx');
+            tc.verifyEmpty(opts.strctident)
+            opts.IRFt = 5;
+            opts.strctident.enddateIV = '2006m7';
+            opts.IRFt = 6;
+            tc.verifyEqual(opts.strctident.enddateIV, '2006m7');
+
         end
     end
-    
+
     methods (Access = private)
-        
+
         function icheckHyperparamLimits(tc, s, t)
             for i = 1 : height(t)
                 tc.verifyEqual( s.(t.param(i)), t.defaultValue(i));
@@ -178,7 +206,7 @@ classdef tSettings < matlab.unittest.TestCase
                     fcn = @() setProp(s, t.param(i), -inf);
                     tc.verifyError(fcn, 'MATLAB:validators:mustBeGreaterThanOrEqual')
                 end
-                
+
                 if isinf(t.upperBound(i))
                     s.(t.param(i)) = inf;
                     tc.verifyEqual( s.(t.param(i)), inf );
@@ -187,9 +215,9 @@ classdef tSettings < matlab.unittest.TestCase
                     tc.verifyError(fcn, 'MATLAB:validators:mustBeLessThanOrEqual')
                 end
             end
-            
+
         end
-        
+
     end
 
 end
