5a6decf93f5987e3e50433c91b539b2110e1a600
ebenetce <ebenetce@host.docker.internal>
2023-03-22 13:27:59 +0000

    amended tests for irfexo

--- tests/tIRFexo.m
+++ tests/tIRFexo.m
@@ -4,7 +4,7 @@ classdef tIRFexo < matlab.unittest.TestCase
     
     methods (Test, TestTags = {'Git'})
 
-        function tIRFforExo(tc)
+        function tPanelIRFforExo(tc)
             
             import matlab.unittest.fixtures.TemporaryFolderFixture            
             tempFixture = tc.applyFixture(TemporaryFolderFixture);
@@ -12,7 +12,7 @@ classdef tIRFexo < matlab.unittest.TestCase
             resultsFile = "newTest";
             
             opts = BEARsettings('PANEL', 'ExcelFile', fullfile(fullfile(bearroot(),'default_bear_data.xlsx')), ...
-                'panel', 'Random_hierarchical');
+                'panel', 'Random_hierarchical', 'FEVD', 0, 'HD', 0, 'F', 0);
             opts.results_path = tempFixture.Folder;
             opts.results_sub = 'newTest';
             opts.plot = false;
@@ -22,6 +22,37 @@ classdef tIRFexo < matlab.unittest.TestCase
             d = load(fullfile(tempFixture.Folder, resultsFile + ".mat"));
             tc.verifyTrue(isfield(d, 'exo_irf_estimates'))
 
+            tc.verifyEqual(size(d.exo_irf_estimates), [d.numendo d.m d.N])
+
+        end
+
+        function tBVARIRFforExo(tc)
+            
+            import matlab.unittest.fixtures.TemporaryFolderFixture            
+            tempFixture = tc.applyFixture(TemporaryFolderFixture);
+            
+            resultsFile = "newTest";
+            
+            opts = BEARsettings('BVAR', 'ExcelFile', fullfile(fullfile(bearroot(),'default_bear_data.xlsx')), 'FEVD', 0, 'HD', 0, 'F', 0);
+            opts.results_path = tempFixture.Folder;
+            opts.results_sub = 'newTest';
+            opts.plot = false;
+            opts.varexo = 'Oil';
+            BEARmain(opts);
+            
+            d = load(fullfile(tempFixture.Folder, resultsFile + ".mat"));
+            tc.verifyTrue(isfield(d, 'exo_irf_estimates'))
+
+            tc.verifyEqual(size(d.exo_irf_estimates), [d.numendo d.m])
+
+        end
+
+    end
+
+    methods (TestClassTeardown)
+
+        function closeFigs(~)
+            close(findobj('Tag','BEARresults'))
         end
 
     end
